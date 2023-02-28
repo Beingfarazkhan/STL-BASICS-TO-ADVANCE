15ecfb8563e8d000f3a3677051c4b11b3c23063a
beingfarazkhan <faraz.khan14158124@gmail.com>
2023-03-01 00:28:54 +0530

    Types of Containers

---
+++ Generic Programming/containers.cpp
@@ -0,0 +1,26 @@
+// 1) Sequence containes -
+// Array (1, 2, 3, 4, 5) ,
+// Vectors,
+// List(DLL),
+// Dequeue,
+// Forward_list(SLL)
+
+// 2) Container Adaptors -
+// Although they have underlying sequential containers , all elements are not accessible eg stack
+// Queue,
+// Stack,
+// priority queue
+
+// 3) Associative Containers -
+// Here you store data in a sorted manner
+// Set
+// Multiset
+// Map
+// Multimap
+
+// 4) Unordered Associative Containers -
+// Here you store data in an unsorted manner
+// unordered_Set
+// unordered_Multiset
+// unordered_Map
+// unordered_Multimap
